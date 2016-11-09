8d0380e624749fda9884ecf74775fbdda7161c3a
Jon Robison <narfman0@gmail.com>
2016-11-08 21:58:02 -0500

    -Fix a minor settings staging/prod discrepancy

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/prod.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/prod.py
@@ -1,1 +1,6 @@
 from .staging import *
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/staging.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/staging.py
@@ -10,8 +10,8 @@ INSTALLED_APPS += (
 )
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_AUTO_CREATE_BUCKET = True
 
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}_staging')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
