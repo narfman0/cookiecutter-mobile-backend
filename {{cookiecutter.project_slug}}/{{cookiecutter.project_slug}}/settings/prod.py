9001983f0381fdacf0d13d385c2f3a6d6c38a3aa
Jon Robison <narfman0@gmail.com>
2016-11-21 00:25:39 -0500

    -Just making this stuff explicit - dunno why anyone would want staging and prod servers the same :)

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/prod.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/prod.py
@@ -3,4 +3,22 @@ from .staging import *
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_LOCATION = 'static'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATICFILES_STORAGE = '{{ cookiecutter.project_slug }}.settings.storage.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = '{{ cookiecutter.project_slug }}.settings.storage.MediaStorage'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', '{{ cookiecutter.database_name }}'),
+        'USER': os.environ.get('DATABASE_USER', '{{ cookiecutter.database_user }}'),
+        'PASSWORD': os.environ.get('DATABASE_PASS', '{{ cookiecutter.database_pass }}'),
+        'HOST': os.environ.get('DATABASE_HOST', '{{ cookiecutter.database_host }}'),
+        'PORT': os.environ.get('DATABASE_PORT', '{{ cookiecutter.database_port }}'),
+    }
+}
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/staging.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/staging.py
@@ -8,9 +8,9 @@ INSTALLED_APPS += (
 )
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_AUTO_CREATE_BUCKET = True
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '{{ cookiecutter.aws_storage_bucket_name }}_staging')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
@@ -19,3 +19,14 @@ STATICFILES_STORAGE = '{{ cookiecutter.project_slug }}.settings.storage.StaticSt
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = '{{ cookiecutter.project_slug }}.settings.storage.MediaStorage'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', '{{ cookiecutter.database_name }}_staging'),
+        'USER': os.environ.get('DATABASE_USER', '{{ cookiecutter.database_user }}'),
+        'PASSWORD': os.environ.get('DATABASE_PASS', '{{ cookiecutter.database_pass }}'),
+        'HOST': os.environ.get('DATABASE_HOST', '{{ cookiecutter.database_host }}'),
+        'PORT': os.environ.get('DATABASE_PORT', '{{ cookiecutter.database_port }}'),
+    }
+}
